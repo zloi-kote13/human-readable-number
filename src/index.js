05df61acb6dd82337437d6be630fa89b14af2646
zloi-kote13 <lyallin96@bk.ru>
2022-02-21 13:40:32 +0400

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function toReadable (number) {
-  
+let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+
+function convert_hundreds(num) {
+  if (num > 99) {
+    return ones[Math.floor(num / 100)] + " hundred " + convert_tens(num % 100);
+  } else {
+    return convert_tens(num);
+  }
 }
+
+function convert_tens(num) {
+  if (num < 10) return ones[num];
+  else if (num >= 10 && num < 20) return teens[num - 10];
+  else {
+    return tens[Math.floor(num / 10)] + " " + ones[num % 10];
+  }
+}
+
+function convert(num) {
+  if (num == 0) return "zero";
+  else return convert_hundreds(num);
+}
+
+module.exports = function toReadable (number) {
+    number.toString ()
+    let result = convert(number);
+    if(result.slice(-1) == ' ') {
+      return result.substring(0, result.length - 1)
+    }
+    return result;
+  }
